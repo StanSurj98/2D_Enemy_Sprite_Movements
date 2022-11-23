4490e68500dab8e58ac7ff2134f523da9aab17bf
Stanley Surjanto <stan.surjanto@gmail.com>
2022-11-23 14:50:22 -0700

    moves a black box representing enemy1 down and to the right

--- public/script.js
+++ public/script.js
@@ -9,3 +9,18 @@ CANVAS_HEIGHT = canvas.height = 1000;
 // Let's start with 1 enemy and make sure we understand how it should behave 
 // This is "Polymorphism" concept of OOP 
 
+enemy1 = {
+  x: 0,
+  y: 0,
+  width: 200,
+  height: 200,
+}
+
+const animate = () => {
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT); // ensures we delete old paint at each recursion
+  enemy1.x ++; // Let's say I want to move its x position
+  enemy1.y ++; // Let's say I want to move its y position
+  ctx.fillRect(enemy1.x, enemy1.y, enemy1.width, enemy1.height) // draws at x, y "start" && width, height "how big"
+  requestAnimationFrame(animate); // our handy recursion method
+}
+animate();
