45843db8066f1c96c79061788b32802e8e2a5e15
Stanley Surjanto <stan.surjanto@gmail.com>
2022-11-23 14:58:43 -0700

    refactor enemy factory class and controls

--- public/script.js
+++ public/script.js
@@ -9,18 +9,40 @@ CANVAS_HEIGHT = canvas.height = 1000;
 // Let's start with 1 enemy and make sure we understand how it should behave 
 // This is "Polymorphism" concept of OOP 
 
-enemy1 = {
-  x: 0,
-  y: 0,
-  width: 200,
-  height: 200,
+// enemy1 = {
+//   x: 0,
+//   y: 0,
+//   width: 200,
+//   height: 200,
+// }
+
+// Let's get programmatic and make an Enemy class so we can generate multiple
+class Enemy {
+  constructor(x, y, width, height) {
+    // this.name = name,
+    this.x = x,
+    this.y = y,
+    this.width = width,
+    this.height = height
+  }
+  updateCoords() {
+    this.x++;
+    this.y++;
+  }
+  draw() {
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
 }
+
+const enemy1 = new Enemy(10, 50, 100, 100);
+const enemy2 = new Enemy(70, 30, 175, 250);
 
 const animate = () => {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT); // ensures we delete old paint at each recursion
-  enemy1.x ++; // Let's say I want to move its x position
-  enemy1.y ++; // Let's say I want to move its y position
-  ctx.fillRect(enemy1.x, enemy1.y, enemy1.width, enemy1.height) // draws at x, y "start" && width, height "how big"
+  enemy1.updateCoords();
+  enemy1.draw();
+  enemy2.updateCoords();
+  enemy2.draw();
   requestAnimationFrame(animate); // our handy recursion method
 }
 animate();
